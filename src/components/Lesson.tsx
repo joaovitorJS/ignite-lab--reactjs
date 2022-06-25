f5bb23f4a0df3552ab67a46c222d1f79ddd74b1d
Joao Vitor <joao.vitordeoliveira@hotmail.com>
2022-06-25 15:32:05 -0300

    feat: Aula 3 - Roteamento e player da aula

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from 'phosphor-react'
 import { isPast, format } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import { Link } from 'react-router-dom';
 
 interface LessonProps {
   title: string;
@@ -21,11 +22,11 @@ export function Lesson({
   })
 
   return (
-    <a href="#">
+    <Link to={`/event/lesson/${slug}`} className='group'>
       <span className="text-gray-300">
         {availableDateFormatted}
       </span>
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
           {isLessonAvailable
             ? (
@@ -50,6 +51,6 @@ export function Lesson({
           {title}
         </strong>
       </div>
-    </a>
+    </Link>
   )
 }
